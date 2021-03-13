a5a610705d9d2f82d790686f1da858cb1b396dbe
Mubashir Jamali <mubashirjamali101@gmail.com>
2021-03-14 01:42:35 +0500

    added wordlist and modified a bit

--- hangman.py
+++ hangman.py
@@ -1,51 +1,57 @@
-#IMPORTS
+# IMPORTS
+from wordlist import DATA  # importing 100 random unique meaningful words
 
+# -------------------------------------------
 
-#-------------------------------------------
-
-#GLOBAL VARIABLED & OPERATIONS
-username = "John"                           # default user name
-turns = 10                                  # default turn count is 10
+# GLOBAL VARIABLED & OPERATIONS
 line = "-------------------------------"    # a default string to display line
-character = {
-    "1": "/  \\",
-    "2": "/\\",
-    "3": "\\ /",
-    "4": "| |",
-    "5": "/ \\",
-    "6": "\\()/",
-    "7": "\\()/_|",
-    "8": "/",
-    "9": "",
-    "10": "",
-}
+randomWordList = []
 
-#-------------------------------------------
+# -------------------------------------------
 
-#DEFINED FUNCTIONS
-def welcome():
+# DEFINED FUNCTIONS
+
+
+def welcome():  # Initial intro and few basic function
     username = getUserName()
-    print(f"| WELCOME {username.upper()} |")
-    print(f"{line}","\nENTER quit() OR q() TO EXIT",f"\n{line}")
+    print(f"| WELCOME {username.upper()} TO GUESS THE WORD |")
+    print(f"{line}", "\nENTER quit() OR q() TO EXIT", f"\n{line}")
 
-def getUserName():                          #get the user name
+
+def getUserName():                          # get the user name
     xUser = input("WHAT ARE YOU CALLED: ")
-    if xUser != "":                         #   if username is not empty then set it at
-        return xUser.lower()                #-> username or just let the defualt one
+    if xUser != "":                         # if username is not empty then set it at
+        return xUser.lower()                # -> username or just let the defualt one
+    else:
+        return "Guest"
 
-def decreaseTheTurn(turnsDecrease):         # get the left turns and minus one from them
-    turnsDecrease -= 1
-    return turnsDecrease
+# get the left turns and minus one from them
+# def decreaseTheTurn(turnsDecrease):
+#     turnsDecrease -= 1
+#     return turnsDecrease
 
 
+def getRandomWord():  # function to get new random word
+    return "Dummy"
 
-def main():                                 # main function of the program
+
+def mainLoop():  # main loop of program, runs out on zero turns
+    turns = 10
+    # default turn count is 10
+    newRandomWord = getRandomWord()
+
+    while turns != 0:
+        turns -= 1  # on every iteration decrease a turn
+
+
+def start():  # main starter of the program
     welcome()
+    mainLoop()
 
-#-------------------------------------------
-
-#GLOBALLY CALLED FUNCTIONS
+# -------------------------------------------
 
 
-#-------------------------------------------
+# GLOBALLY CALLED FUNCTIONS
+start()
 
+# -------------------------------------------
